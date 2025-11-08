87a26b8e5ec95bf682f3acc10874c4e02a7e2334
Douglas Silva <id.oug@hotmail.com>
2025-11-08 20:16:06 -0300

    config: adjust settings in jest to accept faker

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,22 @@ import { compilerOptions } from './tsconfig.json'
 
 export default {
   preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
   moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testRegex: '.*\\..*spec\\.ts$',
+  testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
+  transformIgnorePatterns: ['node_modules/(?!(?:@faker-js)/)'],
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
